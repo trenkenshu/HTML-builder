96da8ca3481cee16311479888596aa2aa8dd2f6d
trenkenshu <trenkenshu@gmail.com>
2022-11-04 16:10:39 +0300

    feat: 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('node:path');
+const readline = require('readline');
+const process = require('process');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log('Напиши что-то хорошее:');
+
+rl.on('line', answer => {
+  if( /^exit/.test(answer) ) {
+    console.log('Столько хорошего ни одна консоль не видела :)');
+    process.exit();
+  } else {
+    fs.appendFile(path.join(__dirname, 'text.txt'), answer+' \r\n', function(err) {
+      if(err) return console.log(err);
+      console.log("Молодец! Теперь напиши что-то еще лучше:");
+    });
+  }
+});
+
+rl.on('SIGINT', () => {
+  console.log('Ну, всё. Хорошего по-немногу :)');
+  process.exit();
+});
+
+
+
