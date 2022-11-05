832c5fa434e2490ada8a1495f4703522e74b2b1f
trenkenshu <trenkenshu@gmail.com>
2022-11-05 15:11:09 +0300

    edit: UI in 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,15 +7,19 @@ let secret = path.join(__dirname, 'secret-folder');
 let files = fsProm.readdir(secret, { withFileTypes: true});
 let answer = [];
 
-console.log(`\nFile listing in ${secret}: `)
+console.log(`\n______________________________________________________________________________________\n
+${secret}\n______________________________________________________________________________________\n`);
 files.then((ans) => {
   for(let i = 0; i < ans.length; i++) {
     if(ans[i].isFile()) {
       fs.stat(path.join(secret, ans[i].name), (err, stat) => {
           if(err) throw err;
           console.log(ans[i].name.split('.')[0], '— ' + path.extname(secret + '/' + ans[i].name).split('.')[1], '— ' + stat.size+' bytes');
-        });
+      });
     }
   }
-})
+  return;
+});
+
+
 
