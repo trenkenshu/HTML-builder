979c638de0f13722995724fdab31a62d77407fa4
trenkenshu <trenkenshu@gmail.com>
2022-11-05 16:11:37 +0300

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,41 @@
+const fsProm = require('fs/promises');
+const path = require('node:path');
+const fs = require('fs');
+
+let from = path.join(__dirname, 'files');
+let to = path.join(__dirname, 'files-copy');
+let options = {
+  recursive : true,
+  force     : true
+};
+
+fs.access(to, (err) => {
+  //console.log('access cb:', err);
+  if(err) {
+    console.log('Creating dir, copying files');
+    copyFiles();
+  } else {
+    console.log('Existing dir, actualising');
+    fs.rm(to, {
+      recursive: true
+    }, (err) => {
+      copyFiles();
+    });
+  }
+});
+
+function copyFiles() {
+  fsProm.mkdir(to, {
+    recursive: true
+  })
+  .catch(err => console.log(err.message))
+  .then(() => {
+    fs.cp(from, to, {
+      recursive : true,
+      force     : true
+    }, (res) => {
+      console.log('Files copied!');
+    });
+  });
+}
 
