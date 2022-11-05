e84d76d68cb5f8c995a5af223f0c8df0f228ca32
trenkenshu <trenkenshu@gmail.com>
2022-11-05 13:33:45 +0300

    feat: 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const fsProm = require('fs/promises');
+const path = require('node:path');
+const fs = require('fs');
+
+
+let secret = path.join(__dirname, 'secret-folder');
+let files = fsProm.readdir(secret, { withFileTypes: true});
+let answer = [];
+
+console.log(`\nFile listing in ${secret}: `)
+files.then((ans) => {
+  for(let i = 0; i < ans.length; i++) {
+    if(ans[i].isFile()) {
+      fs.stat(path.join(secret, ans[i].name), (err, stat) => {
+          if(err) throw err;
+          console.log(ans[i].name.split('.')[0], '— ' + path.extname(secret + '/' + ans[i].name).split('.')[1], '— ' + stat.size+' bytes');
+        });
+    }
+  }
+})
+
