edb1aab61b890c86087ec2ab1799bea375ef0a23
trenkenshu <trenkenshu@gmail.com>
2022-11-06 15:03:55 +0300

    refactor: 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,25 +1,37 @@
 const fsProm = require('fs/promises');
 const path = require('node:path');
 const fs = require('fs');
+const { resolve } = require('path');
 
 
 let secret = path.join(__dirname, 'secret-folder');
 let files = fsProm.readdir(secret, { withFileTypes: true});
-let answer = [];
+let stats = [];
+let filesData = [];
+let q = 0;
 
-console.log(`\n______________________________________________________________________________________\n
-${secret}\n______________________________________________________________________________________\n`);
 files.then((ans) => {
+
   for(let i = 0; i < ans.length; i++) {
     if(ans[i].isFile()) {
-      fs.stat(path.join(secret, ans[i].name), (err, stat) => {
-          if(err) throw err;
-          console.log(ans[i].name.split('.')[0], '— ' + path.extname(secret + '/' + ans[i].name).split('.')[1], '— ' + stat.size+' bytes');
-      });
+      stats.push(fsProm.stat(path.join(secret, ans[i].name)));
+      filesData.push({
+          'file name'   : ans[i].name.split('.')[0],
+          'extension'   : path.extname(secret + '/' + ans[i].name).split('.')[1],
+        });
     }
   }
-  return;
+
+  Promise.allSettled(stats).then((results) => {
+    for( let i = 0; i < results.length; i++) {
+      filesData[i]['size, bytes'] = results[i].value.size;
+    }
+    console.table(filesData);
+  });
+
 });
+
+
 
 
 
